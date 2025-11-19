764369ba994b647ed6ec747b403d542581208cd1
Nicolas <Nicolas.jooris@hotmail.fr>
2025-11-19 20:25:20 +0100

    base player and world

---
+++ jam/player/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta: float) -> void:
+	_gestion_deplacement()
+	move_and_slide()
+
+func _gestion_deplacement() -> void:
+	var direction = Vector2.ZERO
+
+	if Input.is_action_pressed("up"):
+		direction.y -= 1
+	if Input.is_action_pressed("down"):
+		direction.y += 1
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+
+	velocity = direction.normalized() * SPEED
