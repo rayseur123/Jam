bb641f22516f8ebc33828174cd64bcc1cea6682e
devan <ernst.devan@gmail.com>
2025-11-21 21:04:11 +0100

    adding system world change

--- jam/menu/stats/menu_stats.gd
+++ jam/menu/stats/menu_stats.gd
@@ -12,4 +12,5 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://worlds/world2/world_2.tscn")
+	get_tree().change_scene_to_file("res://worlds/world" + str(Global.world) + "/world.tscn")
+	
--- jam/player/player.gd
+++ jam/player/player.gd
@@ -25,4 +25,5 @@ func _move() -> void:
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("door") && Global.gain > 0:
 		get_tree().change_scene_to_file("res://menu/stats/menu_stats.tscn")
+		Global.world += 1
 		Global.new_world()
