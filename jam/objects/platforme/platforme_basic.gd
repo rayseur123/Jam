7aebe855de52c2cef6fe4e6ac4fe549758fd988b
Nicolas <Nicolas.jooris@hotmail.fr>
2025-11-22 20:38:42 +0100

    test

--- jam/objects/platforme/platforme_basic.gd
+++ jam/objects/platforme/platforme_basic.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 
+@onready var sprite = $Sprite2D
+@onready var collision = $CollisionShape2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -10,7 +13,9 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
-
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		queue_free()
+		# Physiquement elle ne doit plus être là		
+		var tween = create_tween()
+		tween.tween_property(sprite, "modulate:a", 0.0, 0.5) # fade en 0.5s
+		tween.finished.connect(queue_free)
