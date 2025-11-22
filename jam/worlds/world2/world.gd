3e6366a786e877b192be9c75e832ca06dca1e4a2
devan <ernst.devan@gmail.com>
2025-11-22 20:39:23 +0100

    life share beetween world

---
+++ jam/worlds/world2/world.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$HUD.update_heart()
+	$chest.connect("use_key", $HUD.get_money)
+	$key.connect("key_collected", $HUD.have_key) 
+	$player.connect("lose_heart", $HUD.lose_heart)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+	
